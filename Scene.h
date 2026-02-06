b6e6d9874c1af6fef73d39ca97be742ff8becb31
PreaseP@H <dong123541@gmail.com>
2026-02-06 19:51:12 +0900

    Revert "충돌 체크용 임시 Scene 생성"
    This reverts commit b165a2684879e87d6d0fe0f1d92d09880e91bf4e.

--- Scene.h
+++ Scene.h
@@ -8,20 +8,20 @@ class Scene
 {
 private:
 	std::wstring m_wstrName;
+	std::vector<Object*> arrObj[(UINT)GROUP_TYPE::END];
 
 protected:
 	Camera* m_pCamera;
-	std::vector<Object*> arrObj[(UINT)GROUP_TYPE::END];
 
 public:
 	Scene();
-	virtual ~Scene();
+	~Scene();
 
 	void SetName(const std::wstring& strName) { m_wstrName = strName; }
 	const std::wstring& GetName() { return m_wstrName; }
 
-	virtual void Update();
-	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList);
+	void Update();
+	void Render(ID3D12GraphicsCommandList* pd3dCommandList);
 
 	virtual void Enter(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList) = 0;
 	virtual void Exit() = 0;
--- define.h
+++ define.h
@@ -29,7 +29,6 @@ enum class GROUP_TYPE {
 
 enum class SCENE_TYPE {
 	TEST,
-	COLLISION, // 충돌 테스트 씬
 
 	END,
 };
