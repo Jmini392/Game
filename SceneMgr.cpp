4e30b613086d876ae57f7b4a16ee5f38b455f31a
PreaseP@H <dong123541@gmail.com>
2026-02-27 22:49:51 +0900

    Revert "충돌 체크용 임시 Scene 생성"
    This reverts commit b165a2684879e87d6d0fe0f1d92d09880e91bf4e.

--- SceneMgr.cpp
+++ SceneMgr.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 #include "SceneMgr.h"
 #include "TestScene.h"
-#include "CollisionCheckScene.h"
 
 SceneMgr::SceneMgr()
 	:arrScene()
@@ -23,11 +22,8 @@ void SceneMgr::Init(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCom
 	//arrScene[(UINT)SCENE_TYPE::STAGE_O1] = new Scene_Start;
 	//arrScene[(UINT)SCENE_TYPE::STAGE_O2] = new Scene_Start;
 
-	arrScene[(UINT)SCENE_TYPE::COLLISION] = new CollisionCheckScene;
-	arrScene[(UINT)SCENE_TYPE::COLLISION]->SetName(L"Collision Check Scene");
-
 	//ÇöÀç ¾À ÁöÁ¤
-	currScene = arrScene[(UINT)SCENE_TYPE::COLLISION];
+	currScene = arrScene[(UINT)SCENE_TYPE::TEST];
 	currScene->Enter(pd3dDevice, pd3dCommandList);
 }
 
